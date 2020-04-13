a8c22c371d70b9479bc263aac88d16f6604af222
gjub <554842443@qq.com>
2020-04-13 15:17:19 +0800

    80端口自定义Ribbon IRule （轮询）

--- cloud-consumer-order80/src/main/java/com/gujunbin/myrule/MySelfRule.java
+++ cloud-consumer-order80/src/main/java/com/gujunbin/myrule/MySelfRule.java
@@ -10,15 +10,19 @@ import org.springframework.context.annotation.Configuration;
  * @Date: 2020/4/12 11:38
  * @Description:
  */
+
+
 @Configuration
 public class MySelfRule {
 
     /**
      * 配置类完成后要在主启动类添加 @RibbonClient 使配置生效
+     *
      * @return
      */
+
     @Bean
-    public IRule myRule(){
+    public IRule myRule() {
         return new RandomRule(); //定义为随机
     }
 }
