9519344475552ddb578b02fc53d2623439a43f6a
gjub <554842443@qq.com>
2020-04-14 12:58:37 +0800

    8001 自测服务熔断

--- cloud-provider-hystrix-payment8001/src/main/java/com/gujunbin/springcloud/service/PaymentService.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/gujunbin/springcloud/service/PaymentService.java
@@ -65,9 +65,9 @@ public class PaymentService {
     // 服务熔断
     @HystrixCommand(fallbackMethod = "paymentCircuitBreakerFallback", commandProperties = {
             @HystrixProperty(name = "circuitBreaker.enabled", value = "true"),//是否开启断路器
-            @HystrixProperty(name = "circuitBreaker.requestVolumeThreshold", value = "10"),// 请求次数
-            @HystrixProperty(name = "circuitBreaker.sleepWindowInMilliseconds", value = "10000"),//时间窗口期 "
-            @HystrixProperty(name = "circuitBreaker.errorThresholdPercentage", value = "60")//失败率达到多少后跳闸
+            @HystrixProperty(name = "circuitBreaker.requestVolumeThreshold", value = "10"),// 请求总数阈值
+            @HystrixProperty(name = "circuitBreaker.sleepWindowInMilliseconds", value = "10000"),//快照时间窗
+            @HystrixProperty(name = "circuitBreaker.errorThresholdPercentage", value = "60")//错误百分比阈值
     })
     public String paymentCircuitBreaker(@PathVariable("id") Integer id) {
         if (id < 0) {
