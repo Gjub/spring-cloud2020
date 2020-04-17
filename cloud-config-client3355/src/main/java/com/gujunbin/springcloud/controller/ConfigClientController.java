d46dacebdee815cc34e87fcdc881e9acb0d00b2c
gjub <www.554842443@qq.com>
2020-04-17 12:25:00 +0800

    更改信息 config.info

--- cloud-config-client3355/src/main/java/com/gujunbin/springcloud/controller/ConfigClientController.java
+++ cloud-config-client3355/src/main/java/com/gujunbin/springcloud/controller/ConfigClientController.java
@@ -1,6 +1,7 @@
 package com.gujunbin.springcloud.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,13 +11,14 @@ import org.springframework.web.bind.annotation.RestController;
  * @Description:
  */
 @RestController
+@RefreshScope //实现自动刷新
 public class ConfigClientController {
 
-    @Value("${server.port}")
-    private String serverPort;  //要访问的3344上的信息
+    @Value("${config.info}")
+    private String configInfo;
 
-    @GetMapping("/serverPort")	//请求地址
-    public String getServerPort(){
-        return serverPort;
+    @GetMapping("/configInfo")	//请求地址
+    public String getConfigInfo(){
+        return configInfo;
     }
 }
